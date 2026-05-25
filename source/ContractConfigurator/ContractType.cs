64bcdb09026536b6ba98a4ce977da8b1582e6728
Clayel <videogamer0907@gmail.com>
2026-05-25 17:49:12 -0400

    do not cancel active contract if prestige has changed

--- source/ContractConfigurator/ContractType.cs
+++ source/ContractConfigurator/ContractType.cs
@@ -758,7 +758,15 @@ namespace ContractConfigurator
                 // Check prestige
                 if (prestige.Count > 0 && !prestige.Contains(contract.Prestige))
                 {
-                    throw new ContractRequirementException("Wrong prestige level.");
+                    if (prestige.Count == 1 && contract.ContractState == Contract.State.Active)
+                    {
+                        contract.Prestige = prestige.First();
+                        LoggingUtil.LogInfo(this,  "Setting prestige level to {0} for contract of type {1} ({2}).", contract.Prestige.ToString(), name, contract.Title);
+                    }
+                    else
+                    {
+                        throw new ContractRequirementException("Wrong prestige level.");
+                    }
                 }
 
                 // Do a Research Bodies check, if applicable
